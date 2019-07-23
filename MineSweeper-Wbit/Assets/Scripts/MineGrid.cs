d7fc2527d7bc6d0267140d8b27b2695a2fc3fc0e
Soleyu <soleyu.alpha@gmaill.com>
2019-07-23 00:01:37 -0500

    Added Tiles and Grid Generator

---
+++ MineSweeper-Wbit/Assets/Scripts/MineGrid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MineGrid : MonoBehaviour
+{
+    public TileScript tilePrefab;
+    public int columns = 8;
+    public int rows = 8;
+    public float distanceBetweenTiles = 1.0F;
+    public TileScript[,] elements = new TileScript[rows, columns];
+
+    public List <Vector3> gridPositions = new List<Vector3>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateTiles();
+    }
+
+
+    void CreateTiles()
+    {
+        for  (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                Instantiate(tilePrefab, new Vector3(x,y,0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MineSweeper-Wbit/Assets/Scripts/TileScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public bool isMined = false;
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMined = Random.value < 0.15;
+    }
+
+    public void loadTexture(int adjacentCount) 
+    {
+        if (isMined)
+        {
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+        }
+    }
+
+    public bool isCovered() 
+    {
+    return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    void OnMouseUpAsButton()
+    {
+        if (isMined)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
